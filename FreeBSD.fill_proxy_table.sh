071c1d5c17f8e8f85358d95048c28a90ff364a88
wel <weldpua2008@gmail.com>
2013-10-18 22:42:38 +0300

    chng table name

--- FreeBSD.fill_proxy_table.sh
+++ FreeBSD.fill_proxy_table.sh
@@ -2,7 +2,7 @@
 # 2010.05
 # this file fill proxy table, but I don't save tru version 
 ipfw table 2 flush
-# ${ipfw} add fwd ${proxy_ip}, ${proxy_port} tcp from $lan_customers to 'table(15)' dst-port 80 in via ${int_if}
+# ${ipfw} add fwd ${proxy_ip}, ${proxy_port} tcp from $lan_customers to 'table(2)' dst-port 80 in via ${int_if}
 
 cat /tmp/tcpdump/stat.out|awk '{print $2}'|sort |uniq -u|awk  '{print "1262099350      "$1"  91.203.140.34"}'>/tmp/tcpdump/stat.out.tmp
 cat /tmp/tcpdump/stat.out.tmp>/tmp/tcpdump/stat.out
